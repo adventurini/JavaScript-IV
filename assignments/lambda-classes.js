fb8bd4de3674f1bd02c9ff74346ed2ba66597067
Anthony Venturini <anthonydventurini@gmail.com>
2019-01-24 16:35:18 -0800

    MVP complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,349 +1,172 @@
 // CODE here for your Lambda Classes
 //first commit
 
-class GameObject {
-    constructor(info){
-    this.createdAt = info.createdAt;
-    this.dimensions = info.dimensions;
-    this.name=info.name;
-    }
-    destroy(){
-        return `${this.name} was removed from the game`;
-    };
+class Person {
+  constructor(things){
+    this.name=things.name;
+    this.location=things.location;
+    this.age=things.age;
+    this.gender=things.gender;
   }
-  
-  
-  
-  /*
-    === CharacterStats ===
-    * healthPoints
-    * name
-    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    * should inherit destroy() from GameObject's prototype
-  */
-  
-  class CharacterStats extends GameObject {
-      constructor(stats){
-          super(stats);
-    this.healthPoints = stats.healthPoints;
+  speak() {
+    console.log(`Hello, my name is ${this.name}, I am from ${this.location}`)
   }
-  takeDamage(){
-    return `${this.name} took damage.`;
-  };
-}
-  
-  /*
-    === Humanoid (Having an appearance or character resembling that of a human.) ===
-    * team
-    * weapons
-    * language
-    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    * should inherit destroy() from GameObject through CharacterStats
-    * should inherit takeDamage() from CharacterStats
-  */
-  
-  
-  
-  class Humanoid extends CharacterStats{
-      constructor(atts){
-          super(atts);
-    this.team = atts.team;
-    this.weapons = atts.weapons;
-    this.language = atts.language;
-  }
-  
-  greet(){
-    return `${this.name} offers a greeting in ${this.language}`;
-  };
 }
 
-// ***********************************************************************//
-// ****************CREDITS: CHRISTOPHER J FOSTER OF LAMBDA****************//
-
-//USE ONLY FOR PURPOSE OF CHANGING FROM PROTOTYPE METHOD TO CLASSES/////////
-
-
-
-class Villain extends Humanoid {
-    constructor(weak){
-        super(weak);
-    this.taunt = weak.taunt;
-    this.secretWeapon = weak.secretWeapon;
+class Instructor extends Person{
+  constructor(stuff){
+    super(stuff);
+    this.speciality=stuff.specialty;
+    this.favLanguage=stuff.favLanguage;
+    this.catchPhrase=stuff.catchPhrase;
   }
-
-  normalAttack(target) {
-    let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 3)); // I set the max damage to the Villain's hp / 4 - the less healthy the attacker, the less damage they can inflict.
-    let plural = "points";
-    if (damage === 1) {
-      plural = "point"; // It's nice to see "1 health point" and not "1 health points".
-    }
-    target.healthPoints -= damage;
-    console.log(
-      `${this.name} hits ${target.name} with a ${
-        this.weapons[
-          Math.floor(Math.random() * Math.floor(this.weapons.length)) // This code chooses randomly from among the attacking character's weapons.
-        ]
-      } for ${damage} health ${plural}!`
-    );
-    if (target.healthPoints <= 0) {
-      console.log(`${target.name} has sustained mortal damage!`);
-      console.log(target.destroy());
-    } else {
-      console.log(`${target.name} lives to fight on!`);
-    }
-  };
-  specialAttack(target) {
-    let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 3)); // The special attack is more powerful.
-    let plural = "points";
-    if (damage === 1) {
-      plural = "point";
-    }
-    target.healthPoints -= damage;
-    console.log(
-      `${this.name} hits ${target.name} with a ${
-        this.secretWeapon
-      } for ${damage} health ${plural}!`
-    );
-    if (target.healthPoints <= 0) {
-      console.log(`${target.name} has sustained mortal damage!`);
-      console.log(target.destroy());
-    } else {
-      console.log(`${target.name} lives to fight on!`);
-    }
-  };
-  
-    speakTaunt(){
-    console.log(`${this.name}: ${this.taunt}`);
-  };
+  demo(subject) {
+    console.log(`Today we learn about ${subject}`)
+  }
 }
-  
-  class Hero extends Humanoid {
-    constructor(strong){
-        super(strong);
-    this.battleCry = strong.battleCry;
-    this.superWeapon = strong.superWeapon;
-  }
-  
-    speakBattleCry() {
-    console.log(`${this.name}: ${this.battleCry}`);
-  };
 
-  normalAttack(target) {
-    let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 2.25)); // The Hero's max damage is hp / 2 (instead of the Villain's hp / 4). The gods smile upon the Hero!
-    let plural = "points";
-    if (damage === 1) {
-      plural = "point";
-    }
-    target.healthPoints -= damage;
-    console.log(
-      `${this.name} hits ${target.name} with a ${
-        this.weapons[Math.floor(Math.random() * Math.floor(this.weapons.length))]
-      } for ${damage} health ${plural}!`
-    );
-    if (target.healthPoints <= 0) {
-      console.log(`${target.name} has sustained mortal damage!`);
-      console.log(target.destroy());
-    } else {
-      console.log(`${target.name} lives to fight on!`);
-    }
-  };
-  
-  specialAttack(target) {
-    let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 1.5));
-    let plural = "points";
-    if (damage === 1) {
-      plural = "point";
-    }
-    target.healthPoints -= damage;
-    console.log(
-      `${this.name} hits ${target.name} with a ${
-        this.superWeapon
-      } for ${damage} health ${plural}!`
-    );
-    if (target.healthPoints <= 0) {
-      console.log(`${target.name} has sustained mortal damage!`);
-      console.log(target.destroy());
-    } else {
-      console.log(`${target.name} lives to fight on!`);
-    }
-  };
+class Student extends Person {
+  constructor(tools){
+    super(tools);
+    this.previousBackground=tools.previousBackground;
+    this.className=tools.className;
+    this.favSubjects=tools.favSubjects;
+  }
+  listsSubjects() {
+    console.log(`${this.favSubjects}`)
+  }
+  PRAssignment(subject){
+    console.log(`${this.name} has submitted a PR for ${subject}`)
+  }
+  sprintChallenge(subject){
+    console.log(`${this.name} has begun sprint challenge on ${subject}`)
+  }
+}
+
+class ProjectManagers extends Instructor {
+  constructor(atts){
+    super(atts);
+    this.gradClassName=atts.gradClassName;
+    this.favInstructor=atts.favInstructor;
+  }
+  standup(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standup times!`)
+  }
+  debugsCode(student, subject){
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+  }
 }
 
 
-  
-  
-  
-  // Outside of this exercise, I might have created a "BossHumanoid" constructor, and collapsed Villain and Hero into that. Since they're two different constructors, however, I repeat myself:
 
-  
-//   thePenguin.normalAttack(Batman);
-//   Batman.specialAttack(thePenguin);
-  
-  console.log("\n");
-  
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
-  
-  // I created a Villain object and Hero object above. Now they will fight to the death:
-  
-  function epicBattle(hero, villain) {
-    console.log(
-      `Here begins the epic battle between ${hero.name} and ${villain.name}!` +
-        "\n"
-    );
-    console.log(
-      `Will ${villain.name} prove victorious, or will ${hero.name}'s ${
-        hero.superWeapon
-      } carry the day? Let's find out!` + "\n"
-    );
-    let winner = "";
-    while (!winner) {
-      let attacker = "";
-      let defender = "";
-      let attDef = Math.floor(Math.random() * Math.floor(2));
-      if (attDef === 0) {
-        attacker = hero;
-        defender = villain;
-      } else {
-        attacker = villain;
-        defender = hero;
-      }
-      let normSpec = Math.floor(Math.random() * Math.floor(2));
-      if (normSpec === 0) {
-        attacker.normalAttack(defender);
-      } else {
-        attacker.specialAttack(defender);
-      }
-      if (defender.healthPoints <= 0) {
-        winner = attacker;
-      }
-    }
-    if (winner === hero) {
-      console.log(
-        `${hero.name} has defeated the mighty ${villain.name}! May ${
-          hero.name
-        } live in glory forever!`
-      );
-    } else {
-      console.log(
-        `Alas, ${villain.name} has slain the venerable ${hero.name}! ${
-          villain.name
-        }'s victory will be written in blood in the Evil Book!`
-      );
-    }
-  }
-  
+const anthony = new Person({
+  name: 'Anthony',
+  location: 'Detroit',
+  age: 33,
+  gender: 'male'
+});
 
-  
+const jack = new Person({
+  name: 'Jack',
+  location: 'Plymouth',
+  age: 23,
+  gender: 'male'
+});
+
+const jeff = new Person({
+  name: 'Jeff',
+  location: 'Clair',
+  age: 53,
+  gender: 'male'
+});
+
+const mary = new Instructor({
+  name: 'Mary',
+  location: 'Shelby',
+  age: 57,
+  gender: 'female',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget your aunt`
+});
+
+const john = new Instructor({
+  name: 'John',
+  location: 'San Fransisco',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'Java',
+  specialty: 'Back-end',
+  catchPhrase: `It could happen`
+});
+
+const peggy = new Instructor({
+  name: 'Peggy',
+  location: 'Sterling Heights',
+  age: 55,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'UI',
+  catchPhrase: `To the clouds`
+});
+
+const carmella = new Student({
+  name: 'Carmella',
+  location: 'Shelby',
+  age: 24,
+  gender: 'female',
+  previousBackground: 'waitress',
+  className: 'WEB15',
+  favSubjects: ['Math', 'Science']
+});
+
+const andrew = new Student({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  previousBackground: 'student',
+  className: 'WEB16',
+  favSubjects: ['Political Science', 'History']
+});
+
+const vincent = new Student({
+  name: 'Vincent',
+  location: 'Royal Oak',
+  age: 28,
+  gender: 'male',
+  previousBackground: 'Sales',
+  className: 'WEB17',
+  favSubjects: ['Science', 'Math']
+});
+const ashley = new ProjectManagers({
+  name: 'Ashley',
+  location: 'San Fransisco',
+  age: 36,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'UI',
+  catchPhrase: `To the clouds`,
+  gradClassName: 'CS1',
+  favInstructor: 'Peggy',
+});
+
+const christina = new ProjectManagers({
+  name: 'Christina',
+  location: 'West Branch',
+  age: 30,
+  gender: 'female',
+  favLanguage: 'English',
+  specialty: 'Front-End',
+  catchPhrase: `I am the best`,
+  gradClassName: 'CS2',
+  favInstructor: 'John',
+});
+
+vincent.listsSubjects();
+christina.standup('web17');
+console.log(ashley.name)
+console.log(mary.age)
 
 
 
-
-  
-    
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-  
-  const thePenguin = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 5,
-      height: 1,
-    },
-    healthPoints: 100,
-    name: 'Penguin',
-    team: 'Weird Loner',
-    weapons: [
-      'Umbrella',
-      'Dynamite',
-    ],
-    language: 'English',
-    taunt: "You likely will not live to regret your decision.",
-    secretWeapon: "Poisoned Ceremonial Dagger"
-  })
-  
-  const Batman = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 3,
-      height: 8,
-    },
-    healthPoints: 125,
-    name: 'Batman',
-    team: 'Gotham',
-    weapons: [
-      'Fist',
-      'Hero Belt',
-    ],
-    language: 'English',
-    taunt: "You likely will not live to regret your decision.",
-    secretWeapon: "Poisoned Ceremonial Dagger"
-  })
-  
-  console.log(mage)
-  
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-  
-
-  
-  
- 
-  epicBattle(thePenguin, Batman);
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,216 +1,347 @@
-/* 
 
-Prototype Refactor
-
-1. Copy and paste your code or the solution from yesterday
-
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
-
-*/
-
-
-//inital commit
-
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-
-/*
-  === GameObject ===
-  * createdAt
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
-*/
-
-function GameObject(info) {
-    this.createdAt = info.createdAt;
-    this.dimensions = info.dimensions;
-    this.name=info.name;
+class GameObject {
+  constructor(info){
+  this.createdAt = info.createdAt;
+  this.dimensions = info.dimensions;
+  this.name=info.name;
   }
-  
-  GameObject.prototype.destroy = function () {
+  destroy(){
       return `${this.name} was removed from the game`;
   };
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  
-  /*
-    === CharacterStats ===
-    * healthPoints
-    * name
-    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    * should inherit destroy() from GameObject's prototype
-  */
-  
-  function CharacterStats(stats) {
-    GameObject.call(this, stats);
-    this.healthPoints = stats.healthPoints;
+}
+
+
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * name
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+
+class CharacterStats extends GameObject {
+    constructor(stats){
+        super(stats);
+  this.healthPoints = stats.healthPoints;
+}
+takeDamage(){
+  return `${this.name} took damage.`;
+};
+}
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+
+
+
+class Humanoid extends CharacterStats{
+    constructor(atts){
+        super(atts);
+  this.team = atts.team;
+  this.weapons = atts.weapons;
+  this.language = atts.language;
+}
+
+greet(){
+  return `${this.name} offers a greeting in ${this.language}`;
+};
+}
+
+// ***********************************************************************//
+// ****************CREDITS: CHRISTOPHER J FOSTER OF LAMBDA****************//
+
+//USE ONLY FOR PURPOSE OF CHANGING FROM PROTOTYPE METHOD TO CLASSES/////////
+
+
+
+class Villain extends Humanoid {
+  constructor(weak){
+      super(weak);
+  this.taunt = weak.taunt;
+  this.secretWeapon = weak.secretWeapon;
+}
+
+normalAttack(target) {
+  let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 3)); // I set the max damage to the Villain's hp / 4 - the less healthy the attacker, the less damage they can inflict.
+  let plural = "points";
+  if (damage === 1) {
+    plural = "point"; // It's nice to see "1 health point" and not "1 health points".
   }
-  
-  CharacterStats.prototype.takeDamage = function () {
-    return `${this.name} took damage.`;
-  };
-  Humanoid.prototype = Object.create(CharacterStats.prototype)
-  
-  /*
-    === Humanoid (Having an appearance or character resembling that of a human.) ===
-    * team
-    * weapons
-    * language
-    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    * should inherit destroy() from GameObject through CharacterStats
-    * should inherit takeDamage() from CharacterStats
-  */
-  
-  
-  
-  function Humanoid(atts) {
-    CharacterStats.call(this, atts);
-    this.team = atts.team;
-    this.weapons = atts.weapons;
-    this.language = atts.language;
+  target.healthPoints -= damage;
+  console.log(
+    `${this.name} hits ${target.name} with a ${
+      this.weapons[
+        Math.floor(Math.random() * Math.floor(this.weapons.length)) // This code chooses randomly from among the attacking character's weapons.
+      ]
+    } for ${damage} health ${plural}!`
+  );
+  if (target.healthPoints <= 0) {
+    console.log(`${target.name} has sustained mortal damage!`);
+    console.log(target.destroy());
+  } else {
+    console.log(`${target.name} lives to fight on!`);
   }
-  
-  Humanoid.prototype.greet = function () {
-    return `${this.name} offers a greeting in ${this.language}`;
-  };
-  
-  
-  
-  // Humanoid.prototype = Object.create(Villain.prototype)
-  // Humanoid.prototype = Object.create(Hero.prototype)
-  
-  
-  /*
-   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-   * Instances of CharacterStats should have all of the same properties as GameObject.
-   */
-  
-  // Test you work by un-commenting these 3 objects and the list of console logs below:
-  
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-  
-  const thePenguin = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 5,
-      height: 1,
-    },
-    healthPoints: 100,
-    name: 'Penguin',
-    team: 'Weird Loner',
-    weapons: [
-      'Umbrella',
-      'Dynamite',
-    ],
-    language: 'English',
-  })
-  
-  const Batman = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 3,
-      height: 8,
-    },
-    healthPoints: 125,
-    name: 'Batman',
-    team: 'Gotham',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  })
-  
-  console.log(mage)
-  
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
-  
-  function Villain(weak){
-    Humanoid.call(this, weak);
+};
+specialAttack(target) {
+  let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 3)); // The special attack is more powerful.
+  let plural = "points";
+  if (damage === 1) {
+    plural = "point";
   }
-  
-  Villain.prototype.strike = function(){
-    return Math.random(-2, -10), 1;
+  target.healthPoints -= damage;
+  console.log(
+    `${this.name} hits ${target.name} with a ${
+      this.secretWeapon
+    } for ${damage} health ${plural}!`
+  );
+  if (target.healthPoints <= 0) {
+    console.log(`${target.name} has sustained mortal damage!`);
+    console.log(target.destroy());
+  } else {
+    console.log(`${target.name} lives to fight on!`);
   }
-  Villain.prototype = Object.create(Humanoid.prototype)
-  
-  
-  function Hero(strong){
-    Humanoid.call(this, strong);
+};
+
+  speakTaunt(){
+  console.log(`${this.name}: ${this.taunt}`);
+};
+}
+
+class Hero extends Humanoid {
+  constructor(strong){
+      super(strong);
+  this.battleCry = strong.battleCry;
+  this.superWeapon = strong.superWeapon;
+}
+
+  speakBattleCry() {
+  console.log(`${this.name}: ${this.battleCry}`);
+};
+
+normalAttack(target) {
+  let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 2.25)); // The Hero's max damage is hp / 2 (instead of the Villain's hp / 4). The gods smile upon the Hero!
+  let plural = "points";
+  if (damage === 1) {
+    plural = "point";
   }
-  
-  Hero.prototype.punch = function(){
-    return Math.random(-2, -10), 1;
+  target.healthPoints -= damage;
+  console.log(
+    `${this.name} hits ${target.name} with a ${
+      this.weapons[Math.floor(Math.random() * Math.floor(this.weapons.length))]
+    } for ${damage} health ${plural}!`
+  );
+  if (target.healthPoints <= 0) {
+    console.log(`${target.name} has sustained mortal damage!`);
+    console.log(target.destroy());
+  } else {
+    console.log(`${target.name} lives to fight on!`);
   }
+};
+
+specialAttack(target) {
+  let damage = Math.floor(Math.random() * Math.floor(this.healthPoints / 1.5));
+  let plural = "points";
+  if (damage === 1) {
+    plural = "point";
+  }
+  target.healthPoints -= damage;
+  console.log(
+    `${this.name} hits ${target.name} with a ${
+      this.superWeapon
+    } for ${damage} health ${plural}!`
+  );
+  if (target.healthPoints <= 0) {
+    console.log(`${target.name} has sustained mortal damage!`);
+    console.log(target.destroy());
+  } else {
+    console.log(`${target.name} lives to fight on!`);
+  }
+};
+}
+
+
+
+
+
+// Outside of this exercise, I might have created a "BossHumanoid" constructor, and collapsed Villain and Hero into that. Since they're two different constructors, however, I repeat myself:
+
+
+//   thePenguin.normalAttack(Batman);
+//   Batman.specialAttack(thePenguin);
+
+console.log("\n");
+
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+// I created a Villain object and Hero object above. Now they will fight to the death:
+
+function epicBattle(hero, villain) {
+  console.log(
+    `Here begins the epic battle between ${hero.name} and ${villain.name}!` +
+      "\n"
+  );
+  console.log(
+    `Will ${villain.name} prove victorious, or will ${hero.name}'s ${
+      hero.superWeapon
+    } carry the day? Let's find out!` + "\n"
+  );
+  let winner = "";
+  while (!winner) {
+    let attacker = "";
+    let defender = "";
+    let attDef = Math.floor(Math.random() * Math.floor(2));
+    if (attDef === 0) {
+      attacker = hero;
+      defender = villain;
+    } else {
+      attacker = villain;
+      defender = hero;
+    }
+    let normSpec = Math.floor(Math.random() * Math.floor(2));
+    if (normSpec === 0) {
+      attacker.normalAttack(defender);
+    } else {
+      attacker.specialAttack(defender);
+    }
+    if (defender.healthPoints <= 0) {
+      winner = attacker;
+    }
+  }
+  if (winner === hero) {
+    console.log(
+      `${hero.name} has defeated the mighty ${villain.name}! May ${
+        hero.name
+      } live in glory forever!`
+    );
+  } else {
+    console.log(
+      `Alas, ${villain.name} has slain the venerable ${hero.name}! ${
+        villain.name
+      }'s victory will be written in blood in the Evil Book!`
+    );
+  }
+}
+
+
+
+
+
+
+
+
   
-  Hero.prototype = Object.create(Humanoid.prototype)
-  
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+const thePenguin = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 5,
+    height: 1,
+  },
+  healthPoints: 100,
+  name: 'Penguin',
+  team: 'Weird Loner',
+  weapons: [
+    'Umbrella',
+    'Dynamite',
+  ],
+  language: 'English',
+  taunt: "You likely will not live to regret your decision.",
+  secretWeapon: "Poisoned Ceremonial Dagger"
+})
+
+const Batman = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 3,
+    height: 8,
+  },
+  healthPoints: 125,
+  name: 'Batman',
+  team: 'Gotham',
+  weapons: [
+    'Fist',
+    'Hero Belt',
+  ],
+  language: 'English',
+  taunt: "You likely will not live to regret your decision.",
+  secretWeapon: "Poisoned Ceremonial Dagger"
+})
+
+console.log(mage)
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+
+
+
+
+epicBattle(thePenguin, Batman);
